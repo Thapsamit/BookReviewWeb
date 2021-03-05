b150ff72228fbb749ee93eebb018892f12d9971c
Amit thapliyal <thapliyalamit2001@gmail.com>
2021-03-05 15:07:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,url_for,request
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/register')
+
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
