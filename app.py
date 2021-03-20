424893137d464cb5a9b26ef8f4c9651b1b7dd65a
Amit thapliyal <thapliyalamit2001@gmail.com>
2021-03-20 16:14:18 +0530

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,url_for,request
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 
 def index():
@@ -13,10 +15,30 @@ def register():
     return render_template('register.html')
 
 
+
 @app.route('/login')
 
 def login():
     return render_template('login.html')
+
+
+    
+@app.route('/dashboard')
+
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/dashboard/search')
+def search():
+    return render_template('search.html') 
+
+@app.route('/dashboard/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/info/<string:isbn>')
+def info(isbn):
+    return render_template('info.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,22 @@
+/*
+const searchBook = document.getElementById('search-book');
+const searchBy = document.getElementById('search-by');
+
+const profileInputs = document.querySelectorAll(".profile-inputs");
+const profileEdit = document.getElementById('profile-edit');
+const profileSave = document.getElementById('profile-save');
+
+
+searchBook.addEventListener("change",()=>{
+   searchBy.setAttribute('placeholder',`Search By ${searchBook.value}`)
+});
+
+
+
+profileEdit.addEventListener('click',()=>{
+  console.log('edit')    
+})
+profileSave.addEventListener('click',()=>{
+  console.log('save')  
+})
+*/
